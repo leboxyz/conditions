cda520c36b070833bab1dea417b66aa3866b287e
unknown <mawayalebo@gmail.com>
2022-04-07 23:01:33 +0200

    initial commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+//main class
+class Main {
+    //main method
+    public static void main(String[] args) {
+        //make condition statement
+        int x = 10;
+        int y = 16;
+
+        //if statement
+        if (x > y) {
+            System.out.println("x is greater than y");
+        } 
+        if(x < y){
+            System.out.println("x is less than y");
+        }
+        if(x == y){
+            System.out.println("x is equal to y");
+        }
+
+    }
+}
